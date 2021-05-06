b741e6ecc7af5df57a1fbf001290d8caf941f29b
darkcross174 <dark-cross@rambler.ru>
2021-05-06 14:18:25 +0500

    add localdate

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -3,6 +3,7 @@ package ru.netology;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.time.LocalDate;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
@@ -14,6 +15,9 @@ public class CardDeliveryTest {
     void shouldSubmitForm() {
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Челябинск");
+        LocalDate today = LocalDate.now();
+        String dayVisit = dayVisit (LocalDate.now(plusDays(3)));
+        $("[data-test-id=date] input").setValue(dayVisit);
         $("[data-test-id=name] input").setValue("Андреев Алексей");
         $("[data-test-id=phone] input").setValue("+79525140000");
         $("[data-test-id=agreement]").click();
