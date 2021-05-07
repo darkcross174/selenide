71d188454c7c19376dd8114acfac0e12d8abbd84
darkcross174 <dark-cross@rambler.ru>
2021-05-07 21:19:53 +0500

    localdate

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,10 +1,14 @@
 package ru.netology;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
+import java.util.Date;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
@@ -16,20 +20,14 @@ public class CardDeliveryTest {
     void shouldSubmitForm() {
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Челябинск");
-        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
-        Calendar today = Calendar.getInstance();
-        today.add(Calendar.DAY_OF_MONTH, 3);
-        $("[data-test-id=date] input").setValue(String.valueOf(today));
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL+"A"+Keys.DELETE);
+        LocalDate reservation = LocalDate.now().plusDays(3);
+        String dayVisit = reservation.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[data-test-id=date] input").setValue (dayVisit);
         $("[data-test-id=name] input").setValue("Андреев Алексей");
         $("[data-test-id=phone] input").setValue("+79525140000");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
         $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
     }
-
-
-
-
-
-
 }
